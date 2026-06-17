539d2c0409dc9b0ee9a60f44bf930ad003f6f31d
Kamil.Mathea <kamilmathe@gmail.com>
2026-06-17 21:15:18 +0200

    eat: set up basic game world, character, and chicken classes

--- js/game.js
+++ js/game.js
@@ -1,7 +1,10 @@
 let canvas;
-let character = new Image();
+let ctx;
+let world = new World();
 
 function init() {
-    canvas = document.getElementById("canvas");
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log('my character is', world.character);
 }
---
+++ models/movable-object.class.js
@@ -0,0 +1,13 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    moveRight() {
+        console.log('move right');
+    }
+
+    moveLeft() {
+        console.log('move left');
+    }
+}
