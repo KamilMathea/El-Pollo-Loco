187185c2133857682bdc504bdc0d1fcca64f02f8
Kamil.Mathea <kamilmathe@gmail.com>
2026-06-18 17:16:01 +0200

    feat: introduce BackgroundObject and refactor drawing pipeline in World

--- models/world.class.js
+++ models/world.class.js
@@ -1,32 +1,43 @@
-class World {
-    character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud(),
-    ];
-    canvas;
-    ctx;
+    class World {
+        character = new Character();
+        enemies = [
+            new Chicken(),
+            new Chicken(),
+            new Chicken()
+        ];
+        clouds = [
+            new Cloud(),
+        ];
+        backgroundObjects = [
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100),
+        ];
+        canvas;
+        ctx;
 
-    constructor(canvas) {
-        this.ctx = canvas.getContext('2d');
-        this.canvas = canvas;
-        this.draw();
+        constructor(canvas) {
+            this.ctx = canvas.getContext('2d');
+            this.canvas = canvas;
+            this.draw();
+        }
+
+        draw() {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            this.addToMap(this.character);
+            this.addObjectsToMap(this.clouds);
+            this.addObjectsToMap(this.enemies);
+            this.addObjectsToMap(this.backgroundObjects);
+
+            requestAnimationFrame(() => this.draw());
+        }
+
+        addObjectsToMap(objects) {
+            objects.forEach(o => {
+                this.addToMap(o);
+            });
+        }
+
+        addToMap(movableObject) {
+            this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+        }
     }
-
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
-
-        requestAnimationFrame(() => this.draw());
-    }
-}
