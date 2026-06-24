449141c7d83655259bbe0e77eed8d79c6f4793f1
Kamil.Mathea <kamilmathe@gmail.com>
2026-06-24 17:38:06 +0200

    feat: implement DrawableObject base abstraction and UI StatusBar

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,20 +17,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.y < 130
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
     }
 
     hit() {
@@ -64,19 +43,6 @@ class MovableObject {
             this.y + this.height > MovableObject.y &&
             this.x < MovableObject.x &&
             this.y < MovableObject.y + MovableObject.height;
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
