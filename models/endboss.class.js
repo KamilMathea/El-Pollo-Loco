8b53c2dd6d0ee1b10f1c7dacfe5417d49c561612
Kamil.Mathea <kamilmathe@gmail.com>
2026-06-30 19:07:18 +0200

    feat: add hurt and death animations for Endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -31,6 +31,19 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/3_attack/G19.png',
         'img/4_enemie_boss_chicken/3_attack/G20.png'
     ];
+
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png'
+    ];
+
     hadFirstContact = false;
     isAlertFinished = false;
     isAttacking = false;
@@ -42,22 +55,38 @@ class Endboss extends MovableObject {
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_ALERT);
         this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 2500;
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            if (this.hadFirstContact && this.isAlertFinished && !this.isAttacking) {
+            if (this.hadFirstContact && this.isAlertFinished && !this.isAttacking && !this.isDead() && !this.isHurt()) {
                 this.moveLeft();
             }
         }, 1000 / 60);
 
         let alertFrameCounter = 0;
         let attackFrameCounter = 0;
+        let deadFrameCounter = 0;
 
         setInterval(() => {
-            if (!this.hadFirstContact) {
+            if (this.isDead()) {
+                if (deadFrameCounter < this.IMAGES_DEAD.length) {
+                    let path = this.IMAGES_DEAD[deadFrameCounter];
+                    this.img = this.imageCache[path];
+                    deadFrameCounter++;
+                } else {
+                    let path = this.IMAGES_DEAD[this.IMAGES_DEAD.length - 1];
+                    this.img = this.imageCache[path];
+                }
+
+            } else if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+
+            } else if (!this.hadFirstContact) {
                 this.loadImage('img/4_enemie_boss_chicken/1_walk/G1.png');
 
             } else if (this.hadFirstContact && !this.isAlertFinished) {
@@ -86,7 +115,7 @@ class Endboss extends MovableObject {
     }
 
     triggerAttack() {
-        if (!this.isAttacking && this.isAlertFinished) {
+        if (!this.isAttacking && this.isAlertFinished && !this.isDead() && !this.isHurt()) {
             this.isAttacking = true;
         }
     }
